69eec3f0f5fc940db3346e1b7badac1ee48411b4
graematterneuro <prussan@proton.me>
2026-08-28 11:48:26 +1000

    Create eeg_icainterp.m
    Function to interpolate missing channels and component maps after ICA decomposition, written by Jason Palmer and first shared on the eeglab mailing list: https://sccn.ucsd.edu/pipermail/eeglablist/2026/019041.html

---
+++ eeg_icainterp.m
@@ -0,0 +1,48 @@
+% function EEGout = eeg_icainterp(EEG,urchanlocs)
+%
+% Author: Jason Palmer, West Virginia University, 2026
+% https://sccn.ucsd.edu/pipermail/eeglablist/2026/019041.html
+%
+% Interpolate missing channels of EEG.icawinv (ICA maps) and EEG data
+%
+% Inputs:
+%
+%     EEG         -     a dataset with ICA from data with rejected / missing channels
+%     urchanlocs  -     array of channel locations with missing channels
+%
+% Outputs:
+%
+%     EEGout        -    dataset with ICA maps and data with rejected /
+%                        missing channels interpolated
+%
+
+function EEGout = eeg_icainterp(EEG,urchanlocs)
+
+% get the original channel numbers
+for k = 1:EEG.nbchan
+    v(k) = EEG.chanlocs(k).urchan;
+end
+
+% create fake dataset to interpolate ica maps
+EEGtmp = EEG;
+EEGtmp.data = EEGtmp.icawinv;
+EEGtmp.trials = 1;
+EEGtmp.pnts = size(EEGtmp.icawinv,2);
+EEGtmp2 = eeg_interp(EEGtmp,urchanlocs);
+
+% copy the icaact and interpolated icawinv into output
+EEGout = EEG;
+EEGout.nbchan = length(urchanlocs);
+EEGout.icawinv = EEGtmp2.data;
+EEGout.icaact = EEG.icaact;
+
+% add zero columns to icasphere where (new) interpolated data channels are
+EEGout.icasphere(:,v) = EEG.icasphere;
+EEGout.icasphere(:,setdiff(1:EEGout.nbchan,v)) = 0;
+
+% add data with interpolated channels
+EEGout.data = reshape(EEGout.icawinv * double(EEGout.icaact(:,:)),EEGout.nbchan,EEGout.pnts,EEGout.trials);
+
+% copy full chanlocs and icachansind
+EEGout.chanlocs = urchanlocs;
+EEGout.icachansind = 1:EEGout.nbchan;
